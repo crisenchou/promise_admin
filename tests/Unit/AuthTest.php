cd87cd120ac77c80e7dba9bc239b0e5127c9e463
crisenchou <269298762@qq.com>
2017-03-31 15:49:26 +0800

    test user case and composer.json

---
+++ tests/Unit/AuthTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AuthTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function login(){
+    }
+}
