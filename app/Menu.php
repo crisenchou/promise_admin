b48714d17f2af759249a0976aee5bc196861dd3d
crisen@crisen.org <crisen@crisen.org>
2017-04-13 16:06:13 +0800

    rabc

--- app/Menu.php
+++ app/Menu.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
 {
-    //
+
+
+    /**
+     * The roles that belong to the user.
+     */
+    public function permission()
+    {
+        return $this->belongsTo('App\Permission');
+    }
 }
--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'description'
+    ];
+
+
+    /**
+     * The roles that belong to the user.
+     */
+    public function permissions()
+    {
+        return $this->belongsToMany('App\Permission');
+    }
 }
