aea1b42a1cfa7dd4e59cce9a583f1e08fe9aaa1b
crisenchou <crisen@crisen.org>
2017-05-09 11:42:05 +0800

    config

---
+++ resources/views/home/profile.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('title',$title)
+@section('page-content')
+    <div class="row">
+        <div class="col-md-12">
+            <!-- PAGE CONTENT BEGINS -->
+            <div class="panel panel-default">
+                <div class="panel-heading">profile</div>
+                <div class="panel-body">
+                    个人中心
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/widget/sidebar.blade.php
+++ resources/views/widget/sidebar.blade.php
@@ -1,6 +1,9 @@
-<div id="sidebar" class="sidebar                  responsive                    ace-save-state">
+<div id="sidebar" class="sidebar responsive  ace-save-state">
     <script type="text/javascript">
-        try{ace.settings.loadState('sidebar')}catch(e){}
+        try {
+            ace.settings.loadState('sidebar')
+        } catch (e) {
+        }
     </script>
 
     <div class="sidebar-shortcuts" id="sidebar-shortcuts">
@@ -37,6 +40,7 @@
     @include('widget.menus')
 
     <div class="sidebar-toggle sidebar-collapse" id="sidebar-collapse">
-        <i id="sidebar-toggle-icon" class="ace-icon fa fa-angle-double-left ace-save-state" data-icon1="ace-icon fa fa-angle-double-left" data-icon2="ace-icon fa fa-angle-double-right"></i>
+        <i id="sidebar-toggle-icon" class="ace-icon fa fa-angle-double-left ace-save-state"
+           data-icon1="ace-icon fa fa-angle-double-left" data-icon2="ace-icon fa fa-angle-double-right"></i>
     </div>
 </div>
