26937430f13688ab229bee849fdf8b2ad9ca4c8e
crisenchou <crisen@crisen.org>
2017-10-12 12:47:39 +0000

    translate

---
+++ resources/lang/zh/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码错误',
+    'reset' => '您的密码已经修改,请重新登录!',
+    'sent' => '您的修改密码连接地址已经发送到了您的邮箱,请查收!',
+    'token' => '重置密码的凭证错误',
+    'user' => "用户名不匹配",
+];
