3d53895d9fd4b7ad04c3b35f8e30e04cfbe5e61c
crisenchou <crisen@crisen.org>
2017-11-02 02:51:00 +0000

    role seed

---
+++ tests/Feature/RouteTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class RouteTest extends TestCase
+{
+    
+    public function testHome()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    public function testLogin()
+    {
+        $response = $this->get('login');
+        $response->assertStatus(200);
+    }
+
+    public function testRegister()
+    {
+        $response = $this->get('register');
+        $response->assertStatus(200);
+    }
+
+    public function testCaptcha()
+    {
+        $response = $this->get('captcha' . mt_rand(1000, 9999));
+        $response->assertStatus(200);
+    }
+}
